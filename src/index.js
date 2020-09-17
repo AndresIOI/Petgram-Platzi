09eeb20f897fe42ff3dea16970b7eaeb90df3b39
AndresIOI <andres_ioi@hotmail.com>
2020-09-17 00:22:54 -0500

    Emoji Dog Face

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-ReactDom.render(<h1>Curso platzi avanzado</h1>, document.getElementById('app'));
+ReactDom.render(<h1>Petgram 🐶 </h1>, document.getElementById('app'));
