ccc91b16f39eefd3682d80665b7c23aba6695725
AndresIOI <andres_ioi@hotmail.com>
2020-09-20 11:03:28 -0500

    Se agrego evento scroll en categorias

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
-import { categories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
- return(
-  <List>
-   {
-    categories.map(category => {
-     return(
-      <Item key={category.id}>
-       <Category  {...category}/>
-      </Item>
-     );
-    })
+
+ const [categories, setCategories] = useState([]);
+ const [showFixed, setShowFixed] = useState(false);
+
+ useEffect( () => {
+  window.fetch('https://petgram-server-4r4xqom0r.vercel.app/categories')
+   .then(response => response.json())
+   .then(categories => {
+    setCategories(categories);
+   })
+   .catch(error => console.log(error));
+ }, []);
+
+ useEffect(()=> {
+   const onScroll = e => {
+    const newShowFixed =  window.scrollY > 180;
+    showFixed !== newShowFixed && setShowFixed(newShowFixed);
    }
-  </List>
+
+   document.addEventListener('scroll', onScroll);
+
+   return () => document.removeEventListener('scroll', onScroll);
+
+ },[showFixed]);
+
+ const renderList = (fixed) => (
+  <List className={fixed ? 'fixed' : ''}>
+ {
+  categories.map(category => {
+   return(
+    <Item key={category.id}>
+     <Category  {...category}/>
+    </Item>
+   );
+  })
+ }
+</List>
+ );
+
+ return(
+   <>
+    { renderList() }
+    { showFixed && renderList(true) }
+  </>
  );
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,12 +1,29 @@
 import Styled from 'styled-components';
+import { fadeIn } from '../../styles/animation';
 
 export const List = Styled.ul`
  display: flex;
  overflow: scroll;
  width: 100%;
  padding: 0px;
+ &.fixed {
+  background: #fff;
+  border-radius: 60px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  max-width: 400px;
+  padding: 5px;
+  position: fixed;
+  top: -1px;
+  transform: scale(.8);
+  z-index: 1;
+ }
 `;
 
 export const Item = Styled.li`
  padding: 0px 8px;
+ ${fadeIn()}
+
 `;
